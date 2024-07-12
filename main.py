bec017726232a68e499cf8d914be82d3f944e2d5
James <jlb0181@auburn.edu>
2024-07-12 11:15:00 -0500

    Run refined experiment

--- main.py
+++ main.py
@@ -1,32 +1,69 @@
+import random
 from easme.parse import parse_proteins, determine_mutability
 from easme.fitness import calculate_fitness_from_consensus
+from easme.selection import (
+    get_living_population,
+    prune,
+    fitness_proportionate_selection,
+    truncation,
+)
 
+# Import protein data.
 PROTEINS = parse_proteins("data/json/sequences.json", 774)
 MUTABILITY = determine_mutability(PROTEINS)
 
+# Set evolutionary algorithm parameters.
+MAX_POPULATION_SIZE = 100
+MU_NUM_PARENTS = 10
+LAMBDA_NUM_CHILDREN = 10
+NUM_GENERATIONS = 100
+
 
 def main():
-    initial_population = []
+    # Initialize population.
+    population = []
     for protein in PROTEINS:
         if protein.location_of_origin == "Alabama":
-            initial_population.append(protein)
+            population.append(protein)
 
-    for protein in initial_population:
-        for other_protein in initial_population:
+    # Remove duplicates.
+    for protein in population:
+        for other_protein in population:
             if other_protein.dna_sequence_coding == protein.dna_sequence_coding:
-                # Remove other_protein from the list.
-                initial_population.remove(other_protein)
+                population.remove(other_protein)
 
-    for protein in initial_population:
+    # Calculate fitness.
+    for protein in population:
         protein.fitness = calculate_fitness_from_consensus(protein, MUTABILITY)
 
-    for _ in range(100):
-        for protein in initial_population:
-            new_individual = protein.mutate(MUTABILITY)
-            if new_individual:
-                print("New individual added!")
+    # Run evolutionary algorithm.
+    for i in range(NUM_GENERATIONS):
+        print(f"== COMMENCING GENERATION {i+1} ==")
 
-    for protein in initial_population:
+        # Get living population as a flat list.
+        living_population = get_living_population(population)
+
+        # Select parents.
+        parents = fitness_proportionate_selection(living_population, MU_NUM_PARENTS)
+
+        # Generate offspring.
+        for j in range(LAMBDA_NUM_CHILDREN):
+            # Select two random (unique) parents.
+            parents = random.sample(parents, 2)
+            child = parents[0].reproduce(parents[1], MUTABILITY)
+            if child:
+                print(
+                    f"Generated child GEN{i+1}SER{child.serial_number} with fitness {child.fitness}."
+                )
+                child.mutate(MUTABILITY)
+
+        # Select survivors.
+        living_population_with_new_children = get_living_population(population)
+        survivors = truncation(living_population_with_new_children, MAX_POPULATION_SIZE)
+        population = prune(population, survivors)
+
+    # Save population to JSON.
+    for protein in population:
         protein.save_to_json(f"data/out/{protein.name}.json")
 
 
